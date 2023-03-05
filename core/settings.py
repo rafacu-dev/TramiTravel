feaa9b049ff8017a67a4b6dc06ac75c06b9dfd85
rafacu-dev <rafaproyectosfuturos@gmail.com>
2023-03-04 22:35:46 -0500

    n

--- core/settings.py
+++ core/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = True
-DEBUG = 'RENDER' not in os.environ
+#DEBUG = 'RENDER' not in os.environ
 
 ALLOWED_HOSTS = ['10.0.2.2','127.0.0.1','localhost','tramicuba.com','tramitravel.com']
 
@@ -164,7 +164,7 @@ AUTH_USER_MODEL="user.UserAccount"
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-if not DEBUG:# 
+if  DEBUG:# not
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_HOST = os.environ.get('EMAIL_HOST')
     EMAIL_PORT = os.environ.get('EMAIL_PORT')
