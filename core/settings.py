04da63a8b2cffd885a2da3f72c3815c582f27e54
rafacu-dev <rafaproyectosfuturos@gmail.com>
2023-03-09 10:13:17 -0500

    n

--- core/settings.py
+++ core/settings.py
@@ -187,7 +187,7 @@ if not DEBUG:#
     SECURE_SSL_REDIRECT = True
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-    #STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
     STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
