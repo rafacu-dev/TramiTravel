b267c358b9130ebe4f78e364142fa41d5b9aece0
rafacu-dev <rafaproyectosfuturos@gmail.com>
2023-03-08 23:47:27 -0500

    n

--- core/settings.py
+++ core/settings.py
@@ -145,7 +145,7 @@ USE_TZ = False
 STATIC_URL = '/static/'
 
 #MEDIA_ROOT = os.path.join(BASE_DIR, 'media').replace("\ "[0],"/")
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 REST_FRAMEWORK = {
