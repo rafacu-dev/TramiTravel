6e7764c0f873bc04078260d45770a8857f4b88ae
rafacu-dev <rafaproyectosfuturos@gmail.com>
2023-03-09 10:06:56 -0500

    n

--- core/settings.py
+++ core/settings.py
@@ -189,6 +189,7 @@ if not DEBUG:#
 
     #STATIC_ROOT = os.path.join(BASE_DIR, 'static')
     STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+    
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
     #STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
