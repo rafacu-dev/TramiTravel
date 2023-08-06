b3b0d431e0a50f2b20ac8902d901a3e8d8e08b72
rafacu-dev <eddyvalera@myyahoo.com>
2023-08-05 21:23:19 -0400

    n

--- apps/bot/urls.py
+++ apps/bot/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import telegram_webhook,PasmsView
+from .views import telegram_webhook,PasmsView,PasmsViewAll
 
 urlpatterns = [
     path('', telegram_webhook, name='telegram_webhook'),
     path('settings-pasms/', PasmsView.as_view(), name='Pasms'),
+    path('settings-pasms-all/', PasmsViewAll.as_view(), name='PasmsViewAll'),
 ]
